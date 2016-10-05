6cf10f65d9cd127196ee32abc60bb520626eb45f
Dilip pashi <dilip.pashi143@gmail.com>
2016-10-05 15:57:41 +0530

    Java_assignment_2.3
    Java_assignment_2.3

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+/*  Write a program to print the following pattern:
+			  a
+			a b a
+		  a b c b a
+			a b a
+			  a                                   
+*/
+// due to Control Flow Chapter is not compleated still now i have created the pattern using newline and TAB tag in java
+
+
+class Pattern{
+	
+public static void main(String[] args){
+
+// pattern created with the help of newline and TAB tag in java
+System.out.println("\t\t\t a   \n\t\t a  \t b\t a	 \n\t a \t b\t c\t b\t a \n\t\t a\t b \t a \n\t\t\t a");
+}
+
+}
